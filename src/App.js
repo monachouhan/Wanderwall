5eda9be857eb03a81133206f446718b16ffc9db4
bhumika <bhumika@amplework.com>
2022-08-22 12:16:35 +0530

    forgetpassword committed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import logo from './logo.svg';
 import Signup from './screens/Signup';
 import Onboarding from './screens/Onboarding';
 import SaveItinerary from './screens/SaveItinerary';
+import ForgetPassword from './screens/ForgetPassword';
 
 function App() {
   return (
     <div className="App">
      {/* <Onboarding /> */}
      {/* <Signup/> */}
-     <SaveItinerary />
+     {/* <SaveItinerary /> */}
+     <ForgetPassword/>
     </div>
   );
 }
