49809ad46a595addd73f6a88a848b3cee6fbe6f3
monachouhan <mona@amplework.com>
2022-08-22 11:05:38 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css'
-import Signup from './screens/Signup';
-import Onboarding from './screens/Onboarding';
+import logo from "./logo.svg";
+import "./App.css";
+import Signup from "./screens/Signup";
+import Onboarding from "./screens/Onboarding";
 
 function App() {
   return (
     <div className="App">
-     <Onboarding />
-     {/* <Signup/> */}
+      <Onboarding />
+      {/* <Signup/> */}
     </div>
   );
 }
